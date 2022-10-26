2a979cff5a02dbf1d6e50d8ab606191aad9ac00b
Jiyoung <ahn.jiyoung15@gmail.com>
2022-10-26 10:30:05 +0900

    sort

---
+++ src/main/java/sort/SelectionSort.java
@@ -0,0 +1,28 @@
+package sort;
+
+import java.util.Arrays;
+
+public class SelectionSort {
+    public int[] sort(int[] arr){
+        int i = 0;
+        int j = 0;
+
+        for(i=1;i<arr.length;i++){
+            int tmp = arr[i];
+            for(j=i-1; j >= 0; j--) {
+                if (tmp < arr[j]) {
+                    arr[j+1] = arr[j];
+                } else break;
+            }
+            arr[j+1] = tmp;
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {5,8,1,4,3};
+        SelectionSort selectionSort = new SelectionSort();
+        arr = selectionSort.sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+}
