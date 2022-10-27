a903417a41e136492065540f9a5f2a752ad5fbec
Jiyoung <ahn.jiyoung15@gmail.com>
2022-10-27 09:19:29 +0900

    map 사용

--- src/main/java/hash/HashMarathon.java
+++ src/main/java/hash/HashMarathon.java
@@ -1,22 +1,33 @@
 package hash;
 
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.Set;
+import java.util.*;
 
 // 시간 초과
 public class HashMarathon {
     public String solution(String[] participant, String[] completion) {
         String answer = "";
         Set<String> hashSet = new HashSet<>();
+        Map<String, Integer> hashMap = new HashMap<>();
+
         for(String p : participant){
-            hashSet.add(p);
+            if(!hashMap.containsKey(p)){
+                hashMap.put(p,1);
+            }
+            else {
+                hashMap.put(p, hashMap.get(p) + 1);
+            }
         }
         for(String c : completion){
-            hashSet.remove(c);
+            if(hashMap.get(c) == 1){
+                hashMap.remove(c);
+            }
+            else{
+                hashMap.replace(c,hashMap.get(c)-1);
+            }
         }
-        Iterator<String> iter = hashSet.iterator();
-        answer = iter.next();
+        for(String key : hashMap.keySet()){
+            answer = key;
+        }
 
         return answer;
     }
