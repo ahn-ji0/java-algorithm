6c5136bf5d0325385b8d5e01eb97991c26ef5ec4
Jiyoung <ahn.jiyoung15@gmail.com>
2022-10-31 10:14:47 +0900

    for문 수정

--- src/main/java/bruteforce/MockTest.java
+++ src/main/java/bruteforce/MockTest.java
@@ -2,7 +2,7 @@ package bruteforce;
 
 import java.util.Arrays;
 
-//배열로 풀기
+//배열로 풀정
 //https://school.programmers.co.kr/learn/courses/30/lessons/42840
 public class MockTest {
     public int[] maxIndex(int[] count){
@@ -27,24 +27,15 @@ public class MockTest {
         int[] student1 = new int[]{1, 2, 3, 4, 5};
         int[] student2 = new int[]{2,1,2,3,2,4,2,5};
         int[] student3 = new int[]{3,3,1,1,2,2,4,4,5,5};
-        int i = 0;
-        int j = 0;
-        int k = 0;
+
         int[] count = new int[]{0,0,0};
-        for(int answer : answers){
-            if(answer == student1[i]){
-                count[0] ++;
-            }
-            if(answer == student2[j]){
-                count[1] ++;
-            }
-            if(answer == student3[k]){
-                count[2] ++;
-            }
-            i++; j++; k++;
-            if(i >= student1.length) i = 0;
-            if(j >= student2.length) j = 0;
-            if(k >= student3.length) k = 0;
+        for(int i = 0 ; i< answers.length; i++){
+            if(answers[i] == student1[i % student1.length])
+                count[0]++;
+            if(answers[i] == student2[i % student2.length])
+                count[1]++;
+            if(answers[i] == student3[i % student3.length])
+                count[2]++;
         }
         return maxIndex(count);
 
