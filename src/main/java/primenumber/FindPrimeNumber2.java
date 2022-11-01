19da71f96d8065251ae0d2bbcc82c0212426bc0a
Jiyoung <ahn.jiyoung15@gmail.com>
2022-11-01 09:38:52 +0900

    소수 찾기 - 2. n/2 이하의 수로 나누어 보기

--- src/main/java/primenumber/FindPrimeNumber2.java
+++ src/main/java/primenumber/FindPrimeNumber2.java
@@ -9,7 +9,7 @@ public class FindPrimeNumber2 {
         if(num == 2 || num ==3)
             return 1;
 
-        for(int i=2; i <= (int)Math.sqrt(num); i++){
+        for(int i=2; i <= (int)num/2; i++){
             if(num % i == 0){
                 return 0;
             }
