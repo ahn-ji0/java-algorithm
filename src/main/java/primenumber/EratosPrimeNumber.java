5b7512226f5c58fea0314cd13f2e298e82ebd921
Jiyoung <ahn.jiyoung15@gmail.com>
2022-11-02 10:35:44 +0900

    에라토스테네스의 체 - (배열) 수정

--- src/main/java/primenumber/EratosPrimeNumber.java
+++ src/main/java/primenumber/EratosPrimeNumber.java
@@ -5,13 +5,16 @@ public class EratosPrimeNumber {
         int answer = 0;
         boolean[] isComposit = new boolean[n+1];
 
-        for(int i=2; i <= n; i++){
+        for(int i=2; i * i <= n; i++){
             if(isComposit[i]==false){
-                answer ++;
                 for(int j = 2; j <= (int)n / i ; j++){
                     isComposit[j*i]=true;
                 }
             }
+        }
+        for(int i = 2; i <= n; i++){
+            if(isComposit[i]==false)
+                answer++;
         }
 
         return answer;
