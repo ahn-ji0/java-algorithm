43e955a59ebce985b7a23e5f8ac227d75a367729
Jiyoung <ahn.jiyoung15@gmail.com>
2022-11-03 09:27:28 +0900

    에라토스테네스의 체 - 배열 만들기

---
+++ src/main/java/primenumber/EratosPrimeArray.java
@@ -0,0 +1,15 @@
+package primenumber;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EratosPrimeArray {
+    public int solution(int n) {
+        boolean[] list = new boolean[n+1];
+        return 0;
+    }
+    public static void main(String[] args) {
+        EratosPrimeNumber ep = new EratosPrimeNumber();
+        System.out.println(ep.solution(10));
+    }
+}
