74345613b2514b7f530feb7c419dd504a08ef6ed
Jiyoung <ahn.jiyoung15@gmail.com>
2022-11-03 09:31:21 +0900

    에라토스테네스의 체 - 배열 ver 2

--- src/main/java/primenumber/EratosPrimeArray.java
+++ src/main/java/primenumber/EratosPrimeArray.java
@@ -5,8 +5,20 @@ import java.util.List;
 
 public class EratosPrimeArray {
     public int solution(int n) {
-        boolean[] list = new boolean[n+1];
-        return 0;
+        boolean[] isPrime = new boolean[n+1];
+        int count = 0;
+        for(int i=2; i< n+1; i++){
+            isPrime[i]=true;
+        }
+        for(int i=2; i< n+1; i++){
+            if(isPrime[i]==true){
+                count++;
+                for(int j=2; j<= n/i ; j++){
+                    isPrime[i*j] = false;
+                }
+            }
+        }
+        return count;
     }
     public static void main(String[] args) {
         EratosPrimeNumber ep = new EratosPrimeNumber();
