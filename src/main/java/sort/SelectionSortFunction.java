5202fa3fc3893bda66946f1e0d78738010675cab
Jiyoung <ahn.jiyoung15@gmail.com>
2022-11-09 10:28:10 +0900

    선택 정렬 - BiFunction<Integer,Integer,Boolean> 적용

---
+++ src/main/java/sort/SelectionSortFunction.java
@@ -0,0 +1,43 @@
+package sort;
+
+import java.util.Arrays;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+
+public class SelectionSortFunction {
+    private BiFunction<Integer,Integer,Boolean> compare;
+
+    public SelectionSortFunction(BiFunction<Integer, Integer, Boolean> compare) {
+        this.compare = compare;
+    }
+
+    public int[] solution(int[] arr){
+        for (int i = 0; i < arr.length; i++) {
+            int val = arr[i];
+            int valIdx = i;
+            for (int j = i; j < arr.length; j++) {
+                if(compare.apply(arr[j],val)){
+                    val = arr[j];
+                    valIdx = j;
+                }
+            }
+            arr[valIdx] = arr[i];
+            arr[i] = val;
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        BiFunction<Integer,Integer,Boolean> ascendSort = (a, b) -> a < b;
+        BiFunction<Integer,Integer,Boolean> descendSort = (a, b) -> a > b;
+        int[] test = new int[] {7,4,2,10,9,223,111,23,3,39};
+
+        SelectionSortFunction sort = new SelectionSortFunction(ascendSort);
+        int[] arr = sort.solution(test);
+        System.out.println(Arrays.toString(arr));
+
+        sort = new SelectionSortFunction(descendSort);
+        arr = sort.solution(test);
+        System.out.println(Arrays.toString(arr));
+    }
+}
