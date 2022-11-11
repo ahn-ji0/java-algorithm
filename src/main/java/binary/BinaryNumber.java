3958fbfcd40f379cd16c5ae0c8f82fcf236b1336
Jiyoung <ahn.jiyoung15@gmail.com>
2022-11-11 09:50:08 +0900

    이진수 구하기 - 두 가지 경우 비교. string으로 바로 썼을 때 값이 반대로 나오는 현상

--- src/main/java/binary/BinaryNumber.java
+++ src/main/java/binary/BinaryNumber.java
@@ -6,6 +6,7 @@ import java.util.Collections;
 import java.util.List;
 
 public class BinaryNumber {
+
     public String solution(int num) {
         String binaryNum = "";
         while(num >= 1){
@@ -16,8 +17,20 @@ public class BinaryNumber {
         return binaryNum;
     }
 
+    public String solution2(int num) {
+        List<Integer> binary = new ArrayList<>();
+        while(num >= 1){
+            binary.add(num % 2);
+            num = num / 2;
+        }
+        Collections.reverse(binary);
+        String binaryNum = binary.toString().replaceAll("[^0-9]","");;
+        return binaryNum;
+    }
+
     public static void main(String[] args) {
         BinaryNumber binaryNumber = new BinaryNumber();
-        System.out.println(binaryNumber.solution(9));
+        System.out.println(binaryNumber.solution(10));
+//        System.out.println(binaryNumber.solution2(10));
     }
 }
