c09f036e2ab57eaf1d30518b7ad5123ed989c3dd
Jiyoung <ahn.jiyoung15@gmail.com>
2022-11-23 10:23:10 +0900

    코드업 1855: 피보나치 수(재귀 안쓰고)

---
+++ src/main/java/recursion/Fibonacci2.java
@@ -0,0 +1,25 @@
+package recursion;
+
+import java.util.Scanner;
+//without recursion
+public class Fibonacci2 {
+    public static int fibonacci(int n){
+        int curr ;
+        int n2 = 0;
+        int n1 = 1;
+        if(n == 1) return 1;
+
+        for (int i = 2; i <= n; i++) {
+            curr = n1 + n2;
+            n2 = n1;
+            n1 = curr;
+        }
+        return curr;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(fibonacci(n));
+    }
+}
