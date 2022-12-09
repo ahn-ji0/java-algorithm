7ecfd0af3c7e3f81b6ddba3f4fd674c894aa470c
Jiyoung <ahn.jiyoung15@gmail.com>
2022-12-09 09:41:33 +0900

    LCS: dp 배열에 값 저장

--- src/main/java/dp/LCS.java
+++ src/main/java/dp/LCS.java
@@ -5,11 +5,16 @@ public class LCS {
         String str1 = "ABCDCBA"; //열
         String str2 = "DCABDC"; //행
 
-        int[][] dp = new int[str2.length()][str1.length()];
+        int[][] dp = new int[str2.length()+1][str1.length()+1];
 
-        for (int i = 0; i < str2.length(); i++) {
-            for (int j = 0; j < str1.length(); j++) {
+        for (int i = 1; i <= str2.length(); i++) {
+            for (int j = 1; j <= str1.length(); j++) {
                 System.out.printf("str2: %s str1: %s\n",str2.charAt(i),str1.charAt(j));
+                if(str2.charAt(j)==str1.charAt(i)){
+                    dp[i][j] = dp[i-1][j-1] + 1;
+                } else{
+                    dp[i][j] = dp[i-1][j-1];
+                }
             }
         }
 
