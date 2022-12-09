f6840522637bb3079eddacc860691e54f10a6f5f
Jiyoung <ahn.jiyoung15@gmail.com>
2022-12-09 09:17:06 +0900

    LCS: 행, 열 지정

---
+++ src/main/java/dp/LCS.java
@@ -0,0 +1,18 @@
+package dp;
+
+public class LCS {
+    public static void main(String[] args) {
+        String str1 = "ABCDCBA"; //열
+        String str2 = "DCABDC"; //행
+
+        int[][] dp = new int[str2.length()][str1.length()];
+
+        for (int i = 0; i < str2.length(); i++) {
+            for (int j = 0; j < str1.length(); j++) {
+                System.out.printf("str2: %s str1: %s\n",str2.charAt(i),str1.charAt(j));
+            }
+        }
+
+
+    }
+}
