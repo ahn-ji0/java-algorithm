de1d71f25f71ca3796fde571ac597fe7f7f11f4a
Jiyoung <ahn.jiyoung15@gmail.com>
2022-12-14 10:32:09 +0900

    최적의 전략 찾기: 카드 n개 있을 경우

--- src/main/java/dp/OptimalStrategy.java
+++ src/main/java/dp/OptimalStrategy.java
@@ -10,8 +10,20 @@ public class OptimalStrategy {
             this.right = right;
         }
 
+        @Override
+        public String toString() {
+            return "Pair{" +
+                    "left=" + left +
+                    ", right=" + right +
+                    '}';
+        }
+
+        public int sum(){
+            return this.left + this.right;
+        }
+
     }
-    public Pair solution(int[] arr){
+    public void solution(int[] arr){
         Pair[][] dp = new Pair[arr.length][arr.length];
 
         for (int i = 0; i < arr.length; i++) {
@@ -21,7 +33,20 @@ public class OptimalStrategy {
         for (int i = 1; i < arr.length; i++) {
             dp[i-1][i] = new Pair(Math.max(arr[i-1],arr[i]),Math.min(arr[i-1],arr[i]));
         }
-        return new Pair(0,0);
+        for (int k = 2; k < arr.length; k++) {
+            for (int i = 0; k + i < arr.length; i++) {
+                int j = i + k;
+                int sum = dp[i][j - 1].sum() + arr[j];
+                int left = Math.max(dp[i][j - 1].right + arr[j], dp[i + 1][j].right + arr[i]);
+                int right = sum - left;
+                dp[i][j] = new Pair(left,right);
+            }
+        }
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i; j < arr.length; j++) {
+                System.out.println(dp[i][j]);
+            }
+        }
     }
     public static void main(String[] args) {
         int[] arr = {2,7,40,19};
